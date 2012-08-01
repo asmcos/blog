2f8420db40b781977292fb08fb23927a2a002121
icsw <asmcos@gmail.com>
2012-08-02 01:06:00 +0800

    add user login

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -2,6 +2,6 @@
 from uliweb.form import *
 
 class BlogsForm(Form):
-    username = StringField(label="用户名", required=True);
+#    username = StringField(label="用户名", required=True);
     title    = StringField(label="标题", required=True);
     content  = TextField(label="内容", required=True,rows=20,cols=60);
