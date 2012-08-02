b05a032fe3168f4079d3032e4bb7f668d661c833
icsw <asmcos@gmail.com>
2012-08-02 19:33:37 +0800

    add css

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -4,4 +4,4 @@ from uliweb.form import *
 class BlogsForm(Form):
 #    username = StringField(label="用户名", required=True);
     title    = StringField(label="标题", required=True);
-    content  = TextField(label="内容", required=True,rows=20,cols=60);
+    content  = TextField(label="内容", required=True,rows=10,cols=60);
